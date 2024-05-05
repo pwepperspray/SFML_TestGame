6b8700f04d5915e5af3043996e24b1d5a79f588f
pwepperspray <akinomika0015@gmail.com>
2024-05-05 21:31:58 +0530

    Added SFML library

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::Window window(sf::VideoMode(800,600),"my window");
+    while(window.isOpen())
+    {
+	    sf::Event event;
+	    while(window.pollEvent(event))
+	    {
+		    if (event.type == sf::Event::Closed)
+		    {
+			    window.close();
+		    
+		    }
+	    }
+    }
+    return 0;
+}
