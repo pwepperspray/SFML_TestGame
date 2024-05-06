52381f4753570283a74c042d9ec77795b21eccbb
pwepperspray <akinomika0015@gmail.com>
2024-05-06 14:46:26 +0530

    Tested audio module of SFML and drew a circle to check graphic capabilities

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,31 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
-int main()
-{
-    sf::Window window(sf::VideoMode(800,600),"my window");
-    while(window.isOpen())
-    {
-	    sf::Event event;
-	    while(window.pollEvent(event))
-	    {
-		    if (event.type == sf::Event::Closed)
-		    {
-			    window.close();
-		    
-		    }
-	    }
-    }
-    return 0;
+int main() {
+
+	std::cout << "kill me" << std::endl;
+	sf::RenderWindow mainwindow(sf::VideoMode(640,480), "Hello World");
+	sf::CircleShape c(20);
+
+	sf::Music music;
+	if(!music.openFromFile(".\\data\\bgm\\tm.wav")){
+		return -1;
+	}
+	music.play();
+
+	while(mainwindow.isOpen()){
+		sf::Event ev;
+
+		while(mainwindow.pollEvent(ev)){
+			if(ev.type == sf::Event::Closed){
+				mainwindow.close();
+			}
+
+		mainwindow.clear();
+		mainwindow.draw(c);
+	        mainwindow.display();	
+		}
+	}
+	return 0;
 }
--- src/run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-g++ -c main.cpp -IC:\Users\jacques_lully\Desktop\game1\lib\include
-g++ main.o -o main -LC:\Users\jacques_lully\Desktop\game1\lib\lib -lsfml-graphics -lsfml-window -lsfml-system sfml-app.exe
