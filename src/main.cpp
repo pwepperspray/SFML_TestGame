92be3ee63d3c392a62c43925dd5bd3db475d2bfa
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2024-05-07 16:23:30 +0530

    tested displaying textures using sprites, used test assets

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,37 @@
 int main() {
 
 	std::cout << "kill me" << std::endl;
-	sf::RenderWindow mainwindow(sf::VideoMode(640,480), "Hello World");
+	sf::RenderWindow mainwindow(sf::VideoMode(960,540), "Hello World");
 	sf::CircleShape c(20);
+
+	sf::Image bg1;
+	if (!bg1.loadFromFile(".\\testassets\\background_layer_1.png")){
+		return -1;
+	}
+	sf::Image bg2;
+	if (!bg2.loadFromFile(".\\testassets\\background_layer_2.png")){
+		return -1;
+	}
+	sf::Image bg3;
+	if (!bg3.loadFromFile(".\\testassets\\background_layer_3.png")){
+		return -1;
+	}
+
+	
+	sf::Texture t1;	
+	if (!t1.loadFromImage(bg1)){
+		return -1;
+	}
+	sf::Texture t2;
+	if (!t2.loadFromImage(bg2)){
+		return -1;
+	}
+	sf::Texture t3;
+	if (!t2.loadFromImage(bg3)){
+		return -1;
+	}
+
+
 
 	sf::Music music;
 	if(!music.openFromFile(".\\data\\bgm\\tm.wav")){
@@ -22,9 +51,12 @@ int main() {
 				mainwindow.close();
 			}
 
-		mainwindow.clear();
-		mainwindow.draw(c);
-	        mainwindow.display();	
+	mainwindow.clear();
+	sf::Sprite sprite;
+	sprite.setTexture(t1);
+	sprite.setScale(3,4);
+	mainwindow.draw(sprite);
+	mainwindow.display();	
 		}
 	}
 	return 0;
