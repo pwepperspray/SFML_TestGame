3a9f84c1a22ebe60dcb2a86cecaa7048513e35d6
cinnamon_rowll <166984309+pwepperspray@users.noreply.github.com>
2024-05-09 18:02:01 +0530

    Static linked libstdc libraries

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 REM This script runs the compiler and links the libraries in the libs folder
 @echo off
 g++ -c .\src\main.cpp -I.\libs\include
-g++ main.o -o a -L.\libs\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
+g++ main.o -o a -static-libgcc -static-libstdc++ -L.\libs\lib -lsfml-graphics -lsfml-audio -lsfml-window -lsfml-system
